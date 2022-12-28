dce16f358353bbff2783882fc88d4a32267f82fa
Luca Mandelli <luca.part.mand@gmail.com>
2022-12-28 16:11:32 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("aplicação se conectando com o banco de dados");
+console.log("Enviando para o GitHub");
