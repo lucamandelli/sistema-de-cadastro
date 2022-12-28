e7d25a1e3c522c717ba553a6a1631f7484618b8c
Luca Mandelli <luca.part.mand@gmail.com>
2022-12-28 16:21:05 -0300

    voltando para o antes de adicionar as duas linhas do console log

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("aplicação se conectando com o banco de dados");
-console.log("Enviando para o GitHub");
+console.log("Rodando o sistema de gerenciamento de jogos")
